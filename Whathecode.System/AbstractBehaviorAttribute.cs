03654ef9fd4dfccdaac52bc4cf5a877c2049a9e2
Steven Jeuris <steven.jeuris@gmail.com>
2011-10-27 18:18:46 +0200

    Added AbstractBehaviorAttribute. (Forgot adding this in previous commit.)

---
+++ Whathecode.System/AbstractBehaviorAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+
+namespace Whathecode.System
+{
+	/// <summary>
+	///   Allows creating an attribute which contains a behavior, implemented in another class of any possible type.
+	/// </summary>
+	/// <remarks>
+	///   Attributes don't allow generic template arguments and only constant expressions.
+	///   By passing a type argument and arguments for its constructor, it is possible to instantiate any desired type,
+	///   overcoming some of these limitations.
+	/// </remarks>
+	/// <author>Steven Jeuris</author>
+	public abstract class AbstractBehaviorAttribute : Attribute
+	{
+		/// <summary>
+		///   Thy dynamically created instance as specified in the constructor.
+		/// </summary>
+		protected object DynamicInstance { get; private set; }
+
+
+		/// <summary>
+		///   Create a new attribute and initialize a certain type.
+		/// </summary>
+		/// <param name = "dynamicType">The type to initialize.</param>
+		/// <param name = "constructorArguments">The arguments to pass to the constructor of the type.</param>
+		protected AbstractBehaviorAttribute( Type dynamicType, params object[] constructorArguments )
+		{
+			Contract.Ensures( DynamicInstance != null );
+
+			DynamicInstance = Activator.CreateInstance( dynamicType, constructorArguments );
+		}
+	}
+}
