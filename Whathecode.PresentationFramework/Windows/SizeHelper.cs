dab5f9dcaf3ad72cc4150b5daacb0c7201514ec8
Steven Jeuris <steven.jeuris@gmail.com>
2012-02-28 13:11:05 +0100

    Added SizeHelper.

---
+++ Whathecode.PresentationFramework/Windows/SizeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+
+namespace Whathecode.System.Windows
+{
+	/// <summary>
+	///   A helper class to do common <see cref = "Size">Size</see> operations.
+	/// </summary>
+	/// <author>Steven Jeuris</author>
+	public static class SizeHelper
+	{
+		/// <summary>
+		///   The maximum possible size represented by <see cref = "Size">Size</see>.
+		/// </summary>
+		public static Size MaxSize = new Size( double.MaxValue, double.MaxValue );
+	}
+}
