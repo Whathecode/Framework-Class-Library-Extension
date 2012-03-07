970e06b3382b38c69b54b4d8cd36758db2725fa5
Steven Jeuris <steven.jeuris@gmail.com>
2012-03-07 18:34:42 +0100

    Start of generic value converter.

---
+++ Whathecode.PresentationFramework/Windows/Data/AbstractGenericValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace Whathecode.System.Windows.Data
+{
+	/// <summary>
+	///   Class which can be used as a base to create generic <see cref = "IValueConverter" />'s.
+	/// </summary>
+	/// <typeparam name = "TFrom">The type to convert from.</typeparam>
+	/// <typeparam name = "TTo">The type to convert to.</typeparam>
+	/// <author>Steven Jeuris</author>
+	public abstract class AbstractGenericValueConverter<TFrom, TTo> : IValueConverter, IValueConverter<TFrom, TTo>
+	{
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			return Convert( (TFrom)value );
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			return ConvertBack( (TTo)value );
+		}
+
+
+		public abstract TTo Convert( TFrom value );
+		public abstract TFrom ConvertBack( TTo value );
+	}
+}
---
+++ Whathecode.PresentationFramework/Windows/Data/IValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Data;
+
+
+namespace Whathecode.System.Windows.Data
+{
+	/// <summary>
+	///   Provides a way to apply custom logic to a binding. This is the generic version of <see cref = "IValueConverter" />.
+	///   TODO: Could this belong in ComponentModel?
+	/// </summary>
+	/// <typeparam name = "TFrom">The type to convert from.</typeparam>
+	/// <typeparam name = "TTo">The type to convert to.</typeparam>
+	/// <author>Steven Jeuris</author>
+	public interface IValueConverter<TFrom, TTo>
+	{
+		TTo Convert( TFrom value );
+
+		TFrom ConvertBack( TTo value );
+	}
+}
