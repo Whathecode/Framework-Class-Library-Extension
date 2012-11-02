ceb2fa594f11c869b4abcd60d4ff58bab8f03a63
Steven Jeuris <steven.jeuris@gmail.com>
2012-11-02 14:52:54 +0100

    General project reorganizing.

--- Whathecode.System.Aspects.Tests/InitializeEventHandlersAttributeTest.cs
+++ Whathecode.System.Aspects.Tests/InitializeEventHandlersAttributeTest.cs
@@ -72,8 +72,8 @@ namespace Whathecode.Tests.System.Aspects
 			}
 
 			// Constructors at the end of the invocation chain start with 'call' immediately.
-			var untilCall = body.GetILAsByteArray().TakeWhile( b => b != OpCodes.Call.Value );
-			return untilCall.Count() != 0 && !untilCall.All( b =>
+			var untilCall = body.GetILAsByteArray().TakeWhile( b => b != OpCodes.Call.Value ).ToList();
+			return untilCall.Count != 0 && !untilCall.All( b =>
 				b == OpCodes.Nop.Value ||     // Never encountered, but my intuition tells me a no-op would be valid.
 				b == OpCodes.Ldarg_0.Value || // Seems to always precede Call immediately.
 				b == OpCodes.Ldarg_1.Value    // Seems to be added when calling base constructor.
