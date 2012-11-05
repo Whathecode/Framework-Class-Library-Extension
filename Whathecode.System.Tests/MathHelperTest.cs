e3ba59fe0e543494a92effeb9bd13144c4549bbe
Steven Jeuris <steven.jeuris@gmail.com>
2012-11-05 13:56:03 +0100

    Added MathHelper.RoundToNearest.

---
+++ Whathecode.System.Tests/MathHelperTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Whathecode.System;
+
+
+namespace Whathecode.Tests.System
+{
+	/// <summary>
+	///   Unit tests for <see href = "Mathelper" />.
+	/// </summary>
+	/// <author>Steven Jeuris</author>
+	[TestClass]
+	public class MathHelperTest
+	{
+		[TestMethod]
+		public void RoundToNearestTest()
+		{
+			// Different types.
+			RoundToNearestTestHelper<short>( 5, 1, 0, 4, 5 );
+			RoundToNearestTestHelper( 5, 1, 0, 4, 5 );
+			RoundToNearestTestHelper<long>( 5, 1, 0, 4, 5 );
+			RoundToNearestTestHelper<ushort>( 5, 1, 0, 4, 5 );
+			RoundToNearestTestHelper<uint>( 5, 1, 0, 4, 5 );
+			RoundToNearestTestHelper<ulong>( 5, 1, 0, 4, 5 );
+			RoundToNearestTestHelper( 1.5f, 0.5, 0, 1, 1.5f );
+			RoundToNearestTestHelper( 1.5, 0.5, 0, 1, 1.5f );
+			RoundToNearestTestHelper( 1.5m, 0.5m, 0, 1, 1.5m );
+
+			// Multiple.
+			RoundToNearestTestHelper( 5, 11, 10, 14, 15 );
+			RoundToNearestTestHelper( 1.5, 3.5, 3, 4, 4.5 );
+
+			// Negative values.
+			RoundToNearestTestHelper( 5, -4, -5, -1, 0 );
+
+			// Rounding issue.
+			// TODO: The following test has a correct outcome, but due to rounding issues asserts false. Create a test which uses an epsilon comparison?
+			//RoundToNearestTestHelper( 0.3, 1, 0.9, 0.11, 0.12 );
+		}
+
+		static void RoundToNearestTestHelper<T>( T factor, T toRoundDown, T roundDownResult, T toRoundUp, T roundUpResult )
+		{
+			Assert.AreEqual( roundDownResult, MathHelper.RoundToNearest( toRoundDown, factor ) );
+			Assert.AreEqual( roundUpResult, MathHelper.RoundToNearest( toRoundUp, factor ) );
+		}
+	}
+}
--- Whathecode.System/MathHelper.cs
+++ Whathecode.System/MathHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Whathecode.System.Operators;
 
 
 namespace Whathecode.System
@@ -17,6 +18,20 @@ namespace Whathecode.System
 		public static double DegreesToRadians( double degrees )
 		{
 			return degrees * (Math.PI / 180);
+		}
+
+		/// <summary>
+		///   Round a value to the nearest multiple of another value.
+		/// </summary>
+		/// <typeparam name = "T">The type of the value.</typeparam>
+		/// <param name = "value">The value to round to the nearest multiple.</param>
+		/// <param name = "roundToMultiple">The passed value will be rounded to the nearest multiple of this value.</param>
+		/// <returns>A multiple of roundToMultiple, nearest to the passed value.</returns>
+		public static T RoundToNearest<T>( T value, T roundToMultiple )
+		{
+			double factor = CastOperator<T, double>.Cast( roundToMultiple );
+			double result = Math.Round( CastOperator<T, double>.Cast( value ) / factor ) * factor;
+			return CastOperator<double, T>.Cast( result );
 		}
 	}
 }
