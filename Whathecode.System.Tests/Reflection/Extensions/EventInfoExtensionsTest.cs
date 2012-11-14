74a432b0af74a823f1cc88e72e7df1b37666c6d5
Steven Jeuris <steven.jeuris@gmail.com>
2012-11-14 18:46:26 +0100

    Upgrade to .NET 4.5 and added EventInfo extensions.

---
+++ Whathecode.System.Tests/Reflection/Extensions/EventInfoExtensionsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Whathecode.System.Reflection.Extensions;
+
+
+namespace Whathecode.Tests.System.Reflection.Extensions
+{
+	[TestClass]
+	public class EventInfoExtensionsTest
+	{
+		class StaticEvents
+		{
+			#pragma warning disable 67
+			public event Action NonStatic;
+			static public event Action Static;			
+
+			static event Action CustomEvent;
+			public event Action Custom
+			{
+				add { CustomEvent += value; }
+				remove { CustomEvent -= value; }
+			}
+			static public event Action CustomStatic
+			{
+				add { CustomEvent += value; }
+				remove { CustomEvent -= value; }
+			}
+			#pragma warning restore 67
+		}
+
+		[TestMethod]
+		public void IsStaticTest()
+		{
+			Assert.IsFalse( typeof( StaticEvents ).GetEvent( "NonStatic" ).IsStatic() );
+			Assert.IsTrue( typeof( StaticEvents ).GetEvent( "Static" ).IsStatic() );
+			Assert.IsFalse( typeof( StaticEvents ).GetEvent( "Custom" ).IsStatic() );
+			Assert.IsTrue( typeof( StaticEvents ).GetEvent( "CustomStatic" ).IsStatic() );
+		}
+	}
+}
---
+++ Whathecode.System/Reflection/Extensions/Extensions.EventInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+
+namespace Whathecode.System.Reflection.Extensions
+{
+	public static partial class Extensions
+	{
+		/// <summary>
+		///   Returns whether the event is static or not.
+		/// </summary>
+		/// <param name = "source">The event to verify whether it is static or not.</param>
+		/// <returns>True when the event is static; false otherwise.</returns>
+		public static bool IsStatic( this EventInfo source )
+		{
+			// When an event is static, both the add and remove method are static.
+			return source.AddMethod.IsStatic;
+		}
+	}
+}
