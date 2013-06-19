37e1de66924aef4791bee9af558898e197e718f6
Steven Jeuris <steven.jeuris@gmail.com>
2013-06-19 16:46:52 +0200

    Improved documentation.
    '&&' can't easily be used in XAML, so changed example to '||'.

--- Whathecode.PresentationFramework/Windows/Data/ConditionConverter.cs
+++ Whathecode.PresentationFramework/Windows/Data/ConditionConverter.cs
@@ -73,7 +73,7 @@ namespace Whathecode.System.Windows.Data
 
 		/// <summary>
 		///   The expression representing the condition resulting in a boolean.
-		///   It should be formatted according to the NCalc library, with 0-indexed numbers between brackets referring to the bindings of the <see cref = "MultiBinding" />. E.g. '[0] && [1]'.
+		///   It should be formatted according to the NCalc library, with 0-indexed numbers between brackets referring to the bindings of the <see cref = "MultiBinding" />. E.g. '[0] || [1]'.
 		/// </summary>
 		public string Expression { get; set; }
 
