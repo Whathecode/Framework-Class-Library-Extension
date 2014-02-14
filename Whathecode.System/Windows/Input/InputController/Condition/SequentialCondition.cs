aa703057ed5f70609538b645aebbd753d5449747
Steven Jeuris <steven.jeuris@gmail.com>
2014-02-14 15:48:27 +0100

    Added SequantialCondition.

---
+++ Whathecode.System/Windows/Input/InputController/Condition/SequentialCondition.cs
@@ -0,0 +1,37 @@
+﻿namespace Whathecode.System.Windows.Input.InputController.Condition
+{
+	/// <summary>
+	///   Condition which validates to true when a sequence of condition validates in order.
+	/// </summary>
+	/// <author>Steven Jeuris</author>
+	public class SequentialCondition : AbstractCombineCondition
+	{
+		int _currentPosition;
+
+
+		public SequentialCondition( params AbstractCondition[] conditions )
+			: base( conditions )
+		{
+		}
+
+
+		protected override bool InputValidates()
+		{
+			if ( Conditions.Count == 0 )
+			{
+				return false;
+			}
+
+			if ( Conditions[ _currentPosition ].Validates() )
+			{
+				if ( ++_currentPosition == Conditions.Count )
+				{
+					_currentPosition = 0;
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
