f040ebde443696d9588efecaa7fd2f3bc908e204
Steven Jeuris <steven.jeuris@gmail.com>
2014-12-17 13:07:45 +0100

    Added constructors to TupleList.

--- Whathecode.System/Collections/Generic/TupleList.cs
+++ Whathecode.System/Collections/Generic/TupleList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Whathecode.System.Extensions;
 
 
 namespace Whathecode.System.Collections.Generic
@@ -11,6 +12,23 @@ namespace Whathecode.System.Collections.Generic
 	/// <typeparam name="T2">The type of the tuple's second component.</typeparam>
 	public class TupleList<T1, T2> : List<Tuple<T1, T2>>
 	{
+		/// <summary>
+		///   Initializes a new tuple list that is empty.
+		/// </summary>
+		public TupleList()
+		{
+		}
+
+		/// <summary>
+		///   Initializes a new tuple list and adds the passed tuples to it.
+		/// </summary>
+		/// <param name="tuples">The tuple elements to add.</param>
+		public TupleList( IEnumerable<Tuple<T1, T2>> tuples )
+		{
+			tuples.ForEach( Add );
+		}
+
+
 		public void Add( T1 item, T2 item2 )
 		{
 			Add( new Tuple<T1, T2>( item, item2 ) );
@@ -25,6 +43,23 @@ namespace Whathecode.System.Collections.Generic
 	/// <typeparam name="T3">The type of the tuple's third component.</typeparam>
 	public class TupleList<T1, T2, T3> : List<Tuple<T1, T2, T3>>
 	{
+		/// <summary>
+		///   Initializes a new tuple list that is empty.
+		/// </summary>
+		public TupleList()
+		{
+		}
+
+		/// <summary>
+		///   Initializes a new tuple list and adds the passed tuples to it.
+		/// </summary>
+		/// <param name="tuples">The tuple elements to add.</param>
+		public TupleList( IEnumerable<Tuple<T1, T2, T3>> tuples )
+		{
+			tuples.ForEach( Add );
+		}
+
+
 		public void Add( T1 item, T2 item2, T3 item3 )
 		{
 			Add( new Tuple<T1, T2, T3>( item, item2, item3 ) );
@@ -40,6 +75,23 @@ namespace Whathecode.System.Collections.Generic
 	/// <typeparam name="T4">The type of the tuple's fourth component.</typeparam>
 	public class TupleList<T1, T2, T3, T4> : List<Tuple<T1, T2, T3, T4>>
 	{
+		/// <summary>
+		///   Initializes a new tuple list that is empty.
+		/// </summary>
+		public TupleList()
+		{
+		}
+
+		/// <summary>
+		///   Initializes a new tuple list and adds the passed tuples to it.
+		/// </summary>
+		/// <param name="tuples">The tuple elements to add.</param>
+		public TupleList( IEnumerable<Tuple<T1, T2, T3, T4>> tuples )
+		{
+			tuples.ForEach( Add );
+		}
+
+
 		public void Add( T1 item, T2 item2, T3 item3, T4 item4 )
 		{
 			Add( new Tuple<T1, T2, T3, T4>( item, item2, item3, item4 ) );
@@ -56,6 +108,23 @@ namespace Whathecode.System.Collections.Generic
 	/// <typeparam name="T5">The type of the tuple's fifth component.</typeparam>
 	public class TupleList<T1, T2, T3, T4, T5> : List<Tuple<T1, T2, T3, T4, T5>>
 	{
+		/// <summary>
+		///   Initializes a new tuple list that is empty.
+		/// </summary>
+		public TupleList()
+		{
+		}
+
+		/// <summary>
+		///   Initializes a new tuple list and adds the passed tuples to it.
+		/// </summary>
+		/// <param name="tuples">The tuple elements to add.</param>
+		public TupleList( IEnumerable<Tuple<T1, T2, T3, T4, T5>> tuples )
+		{
+			tuples.ForEach( Add );
+		}
+
+
 		public void Add( T1 item, T2 item2, T3 item3, T4 item4, T5 item5 )
 		{
 			Add( new Tuple<T1, T2, T3, T4, T5>( item, item2, item3, item4, item5 ) );
