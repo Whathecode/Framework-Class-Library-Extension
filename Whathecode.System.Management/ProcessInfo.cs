2925f16880107ede52aa059e3dd62c9ab95c2a94
Steven Jeuris <steven.jeuris@gmail.com>
2015-01-31 18:46:06 +0100

    Added a process tracker.

---
+++ Whathecode.System.Management/ProcessInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Whathecode.System.Management
+{
+	public class ProcessInfo
+	{
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+		public string CommandLine { get; private set; }
+
+
+		public ProcessInfo( int id, string name, string commandLine )
+		{
+			Id = id;
+			Name = name;
+			CommandLine = commandLine;
+		}
+
+
+		public override string ToString()
+		{
+			return String.Format( "PID: {0}, Name: {1}, Arguments: {2}", Id, Name, CommandLine );
+		}
+	}
+}
---
+++ Whathecode.System.Management/ProcessTracker.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Management;
+
+
+namespace Whathecode.System.Management
+{
+	public class ProcessTracker : AbstractDisposable
+	{
+		bool _hasStarted;
+		ManagementEventWatcher _startWatcher;
+		ManagementEventWatcher _stopWatcher;
+
+		/// <summary>
+		///   Triggered when processes are run, including the command line parameters when applicable.
+		/// </summary>
+		public event Action<ProcessInfo> ProcessStarted = delegate { };
+		/// <summary>
+		///   Triggered when processes are stopped.
+		/// </summary>
+		public event Action<ProcessInfo> ProcessStopped = delegate { };
+
+
+		public void Start()
+		{
+			var interval = new TimeSpan( 0, 0, 1 );
+			const string isWin32Process = "TargetInstance isa \"Win32_Process\"";
+
+			// Listen for started processes.
+			var startQuery = new WqlEventQuery( "__InstanceCreationEvent", interval, isWin32Process );
+			_startWatcher = new ManagementEventWatcher( startQuery );
+			_startWatcher.Start();
+			_startWatcher.EventArrived += OnStartEventArrived;
+
+			// Listen for closed processes.
+			var stopQuery = new WqlEventQuery( "__InstanceDeletionEvent", interval, isWin32Process );
+			_stopWatcher = new ManagementEventWatcher( stopQuery );
+			_stopWatcher.Start();
+			_stopWatcher.EventArrived += OnStopEventArrived;
+
+			_hasStarted = true;
+		}
+
+		public void Stop()
+		{
+			if ( !_hasStarted )
+			{
+				return;
+			}
+
+			_startWatcher.Stop();
+			_stopWatcher.Stop();
+			_hasStarted = false;
+		}
+
+		void OnStartEventArrived( object sender, EventArrivedEventArgs e )
+		{
+			var o = (ManagementBaseObject)e.NewEvent[ "TargetInstance" ];
+
+			ProcessStarted( RetrieveProcessInfo( o ) );
+		}
+
+		void OnStopEventArrived( object sender, EventArrivedEventArgs e )
+		{
+			var o = (ManagementBaseObject)e.NewEvent[ "TargetInstance" ];
+
+			ProcessStopped( RetrieveProcessInfo( o ) );
+		}
+
+		static ProcessInfo RetrieveProcessInfo( ManagementBaseObject o )
+		{
+			return new ProcessInfo(
+				Convert.ToInt32( o[ "ProcessId" ] ),
+				(string)o[ "Name" ],
+				(string)o[ "CommandLine" ] );
+		}
+
+		protected override void FreeManagedResources()
+		{
+			Stop();
+		}
+
+		protected override void FreeUnmanagedResources()
+		{
+			// Nothing to do.
+		}
+	}
+}
