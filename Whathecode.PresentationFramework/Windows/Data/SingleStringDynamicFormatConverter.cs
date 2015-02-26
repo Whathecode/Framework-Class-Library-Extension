597ad6597031460466c4c0c40eb191686e556c9c
Steven Jeuris <steven.jeuris@gmail.com>
2015-02-26 18:28:13 +0100

    Added/extended string format converters for WPF.

---
+++ Whathecode.PresentationFramework/Windows/Data/SingleStringDynamicFormatConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Whathecode.System.Windows.Markup;
+
+
+namespace Whathecode.System.Windows.Data
+{
+	/// <summary>
+	///   Converter which converts a given object to a string,
+	///   replacing one item in a bound format string with the string representation of the object.
+	///   The first bound object is the object to format, the second bound value is the format string.
+	/// </summary>
+	/// <author>Steven Jeuris</author>
+	public class SingleStringDynamicFormatConverter : AbstractMultiValueConverter<object, string>
+	{
+		public override string Convert( object[] values )
+		{
+			object toFormat = values[ 0 ];
+			string format = values[ 1 ] as string;
+
+			return format != null
+				? String.Format( format, toFormat )
+				: toFormat.ToString();
+		}
+
+		public override object[] ConvertBack( string value )
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+
+	/// <summary>
+	///   Markup extension which returns a converter which converts a given object to a string,
+	///   replacing one item in a bound format string with the string representation of the object.
+	///   The first bound object is the object to format, the second bound value is the format string.
+	/// </summary>
+	public class SingleStringDynamicFormatConverterExtension : AbstractMarkupExtension
+	{
+		protected override object ProvideValue( object targetObject, object targetProperty )
+		{
+			return new SingleStringDynamicFormatConverter();
+		}
+	}
+}
--- Whathecode.PresentationFramework/Windows/Data/SingleStringFormatConverter.cs
+++ Whathecode.PresentationFramework/Windows/Data/SingleStringFormatConverter.cs
@@ -1,20 +1,36 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using Whathecode.System.Windows.Markup;
 
 
 namespace Whathecode.System.Windows.Data
 {
 	/// <summary>
 	///   Converter which converts a given object to a string,
-	///   replacing one item in a specified string passed as converter paramater with the string representation of the object.
+	///   replacing one item in a specified string passed as converter parameter with the string representation of the object.
 	/// </summary>
 	/// <author>Steven Jeuris</author>
 	public class SingleStringFormatConverter : IValueConverter
 	{
+		readonly bool _useParameter;
+		readonly string _format;
+
+
+		public SingleStringFormatConverter()
+		{
+			_useParameter = true;
+		}
+
+		public SingleStringFormatConverter( string format )
+		{
+			_format = format;
+		}
+
+
 		public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
 		{
-			string format = parameter as string;
+			string format = _useParameter ? parameter as string : _format;
 
 			return format != null
 				? String.Format( format, value )
@@ -24,6 +40,25 @@ namespace Whathecode.System.Windows.Data
 		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
 		{
 			throw new NotImplementedException();
+		}
+	}
+
+
+	/// <summary>
+	///   Markup extension which returns a converter which converts a given object to a string,
+	///   replacing one item in a specified string passed as converter parameter with the string representation of the object.
+	/// </summary>
+	public class SingleStringFormatConverterExtension : AbstractMarkupExtension
+	{
+		/// <summary>
+		///   A composite format string used to format the bound value.
+		/// </summary>
+		public string Format { get; set; }
+
+
+		protected override object ProvideValue( object targetObject, object targetProperty )
+		{
+			return new SingleStringFormatConverter( Format );
 		}
 	}
 }
