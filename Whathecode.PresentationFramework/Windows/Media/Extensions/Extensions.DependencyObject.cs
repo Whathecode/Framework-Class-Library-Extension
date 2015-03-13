0ee81ad09640293f037c5e8574167c18bb5c0382
Steven Jeuris <steven.jeuris@gmail.com>
2015-03-13 13:34:24 +0100

    Added 'FindParent' to find parents of specific types in WPF.

---
+++ Whathecode.PresentationFramework/Windows/Media/Extensions/Extensions.DependencyObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+
+namespace Whathecode.System.Windows.Media.Extensions
+{
+	public static partial class Extensions
+	{
+		/// <summary>
+		///   Returns the first parent of a specific type of the passed visual object.
+		/// </summary>
+		/// <typeparam name="T">The type of the parent to look for.</typeparam>
+		/// <param name="reference">The visual whose parent is returned.</param>
+		public static T FindParent<T>( this DependencyObject reference )
+			where T : DependencyObject
+		{
+			DependencyObject parent = VisualTreeHelper.GetParent( reference );
+			if ( parent == null )
+			{
+				return null;
+			}
+			else
+			{
+				return parent is T ? (T)parent : FindParent<T>( parent );
+			}
+		}
+	}
+}
